dccbf9f6bac2988b7d8075dd1244e9f90f12464e
Maxi-Gonzalez <40243164+Maxi-Gonzalez@users.noreply.github.com>
2024-02-24 19:25:42 -0300

    S

--- TerceraPreEntrega/urls.py
+++ TerceraPreEntrega/urls.py
@@ -4,4 +4,6 @@ from TerceraPreEntrega import views
 urlpatterns = [
     path('',views.inicio),
     path('cafes',views.cafes),
+    path('products',views.products),
+    path('about',views.about), 
 ]
--- TerceraPreEntrega/views.py
+++ TerceraPreEntrega/views.py
@@ -3,9 +3,15 @@ from TerceraPreEntrega.models import Cafe
 
 # Create your views here.
 def inicio (request):
-    return render(request,"index.html")
+    return render(request,"TerceraPreEntrega/index.html")
 
 def cafes(request):
     cafes=Cafe.objects.all()  #Esto me trae todo de la base de datos sql
-    
+
     return render(request,"TerceraPreEntrega/cafes.html",{"cafes":cafes})
+
+def products(request):
+    return render(request, "TerceraPreEntrega/products.html")
+
+def about(request):
+    return render(request, "TerceraPreEntrega/about.html")
