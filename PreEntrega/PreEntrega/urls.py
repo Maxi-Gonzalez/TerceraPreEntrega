6805582d86bb5e53d3077fa30ca8c1ecfba897a8
Maxi-Gonzalez <40243164+Maxi-Gonzalez@users.noreply.github.com>
2024-02-23 22:25:31 -0300

    Eliminacion de proyecto

--- PreEntrega/PreEntrega/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path
-from PreEntrega.views import saludo
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("saludo/",saludo)
-]
